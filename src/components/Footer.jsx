d8e742723a82fee0d9e9c7faedb461aae4f3e0d1
kiminagon <chto22089@g.nihon-u.ac.jp>
2023-10-16 17:44:10 +0900

    完成

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>This fake website example is CCO - any part of this code may be reused in any way you wish. Original example written by Chris Mills, 2016</p>
+          <p>
+            <a href="https://dog.ceo/dog-api/about">Dove icon</a> by Lorc
+          </p>
+        </div>
+      </footer>
+    );
+  }
