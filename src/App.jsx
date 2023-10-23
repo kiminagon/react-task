99f5909d6cf4ebaeda56d76769844a659b18a503
kiminagon <chto22089@g.nihon-u.ac.jp>
2023-10-23 15:18:53 +0900

    どうだ！

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+  const name = "Chris";
+  return <p>Player 1: {name}</p>;
 }
