8a1b209787f0947cbeb2d51185612a679ccc6ee2
kiminagon <chto22089@g.nihon-u.ac.jp>
2023-10-23 16:14:53 +0900

    頼んだよ

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,31 @@
-import { useState } from "react";
-
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
 export default function App() {
-  const [previousGuesses, setPreviousGuesses] = useState([]);
-  const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const guessField = event.target.elements.guessField;
-    const newUserGuess = Number(guessField.value);
-    setPreviousGuesses([].concat(previousGuesses, [newUserGuess]));
-    guessField.value = "";
-    guessField.focus();
-  }
-
-  function handleClickResetButton() {
-    setPreviousGuesses([]);
-    setRandomNumber(generateRandomNumber());
-  }
-
-  const guessCount = previousGuesses.length;
-  const userGuess = guessCount === 0 ? null : previousGuesses[guessCount - 1];
-  const gameClear = userGuess === randomNumber;
-  const gameOver = guessCount === 10;
-  const gameFinished = gameClear || gameOver;
-
-  let lastResultMessage = "Wrong!";
-  if (gameClear) {
-    lastResultMessage = "Congratulations! You got it right!";
-  } else if (gameOver) {
-    lastResultMessage = "!!!GAME OVER!!!";
-  }
-  let lastResultColor = "";
-  if (gameClear) {
-    lastResultColor = "green";
-  } else if (userGuess != null) {
-    lastResultColor = "red";
-  }
-
+  const showStory = false;
+  const xItem = "";
+  const yItem = "";
+  const zItem = "";
+  const ukus = "us";
   return (
     <>
-      <h1>Number guessing game</h1>
-
-      <p>
-        We have selected a random number between 1 and 100. See if you can guess
-        it in 10 turns or fewer. We'll tell you if your guess was too high or
-        too low.
-      </p>
-
-      <div class="form">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="guessField">Enter a guess: </label>
-          <input
-            type="number"
-            min="1"
-            max="100"
-            required
-            name="guessField"
-            class="guessField"
-            disabled={gameFinished}
-          />
-          <input
-            type="submit"
-            value="Submit guess"
-            class="guessSubmit"
-            disabled={gameFinished}
-          />
-        </form>
+      <div>
+        <label htmlFor="customname">Enter custom name:</label>
+        <input type="text" placeholder="" />
       </div>
-
-      {userGuess != null && (
-        <div class="resultParas">
-          <p class="guesses">{previousGuesses.join(" ")}</p>
-          <p class="lastResult" style={{ backgroundColor: lastResultColor }}>
-            {lastResultMessage}
-          </p>
-          {userGuess !== randomNumber && (
-            <p class="lowOrHi">
-              {userGuess < randomNumber
-                ? "Last guess was too low!"
-                : "Last guess was too high!"}
-            </p>
-          )}
-        </div>
-      )}
-
-      {gameFinished && (
-        <button onClick={handleClickResetButton}>Start new game</button>
+      <div>
+        <label htmlFor="us">US</label>
+        <input type="radio" value="us" checked={ukus === "us"} />
+        <label htmlFor="uk">UK</label>
+        <input type="radio" value="uk" checked={ukus === "uk"} />
+      </div>
+      <div>
+        <button>Generate random story</button>
+      </div>
+      {showStory && (
+        <p>
+          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
+          got to {yItem}, they stared in horror for a few moments, then {zItem}.
+          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
+          pounds, and it was a hot day.
+        </p>
       )}
     </>
   );
