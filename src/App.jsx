a291a3a72459d7325460b357d3d645ff078606e9
kiminagon <chto22089@g.nihon-u.ac.jp>
2023-10-28 17:45:33 +0900

    もう無理ぽ

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,70 @@
+import { useState } from "react";
+
 export default function App() {
-  const showStory = false;
-  const xItem = "";
-  const yItem = "";
-  const zItem = "";
-  const ukus = "us";
+  const [showStory, setShowStory] = useState(false);
+  const [name, setName] = useState("Bob");
+  const [isUK, setIsUK] = useState(false);
+  const [temperatureUnit, setTemperatureUnit] = useState("fahrenheit");
+  const [temperature, setTemperature] = useState(94);
+  const [weightUnit, setWeightUnit] = useState("pounds");
+  const [weight, setWeight] = useState(300);
+
+  function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+
+  function handleClick() {
+    const inputName = document.getElementById("customname").value;
+    setName(inputName);
+
+    setShowStory(true);
+
+    if (isUK) {
+      setTemperatureUnit("centigrade");
+      setTemperature(34);
+      setWeightUnit("stone");
+      setWeight(21);
+    }else{
+      setTemperatureUnit("fahrenheit");
+      setTemperature(94);
+      setWeightUnit("pounds");
+      setWeight(300);
+    }
+  }
+
+  function handleUKRadioChange() {
+    setIsUK(!isUK);
+  }
+
+  const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+  const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+  const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
   return (
     <>
       <div>
         <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder="" />
+        <input type="text" id="customname" placeholder="" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <label htmlFor="us">US</label>
-        <input type="radio" value="us" checked={ukus === "us"} />
+        <input type="radio" id="us" value="us" checked={!isUK} onChange={handleUKRadioChange} />
         <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} />
+        <input type="radio" id="uk" value="uk" checked={isUK} onChange={handleUKRadioChange} />
       </div>
       <div>
-        <button>Generate random story</button>
+        <button onClick={handleClick}>Generate random story</button>
       </div>
       {showStory && (
         <p>
-          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
+          It was {temperature} {temperatureUnit} outside, so {xItem} went for a walk. When they
           got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
-          pounds, and it was a hot day.
+          {name} saw the whole thing, but was not surprised — {xItem} weighs {weight} {weightUnit}
+          , and it was a hot day.
         </p>
       )}
     </>
